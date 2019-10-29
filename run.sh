e8a1cb2925113553fe866d6de1b2e1c25b0581e8
root <winson.luk@gmail.com>
2019-10-29 19:11:44 +0000

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 while true; do
+  git commit -am "update";git push
   nohup python ~/gpt_summaries/generate_summaries.py $(($(wc -l ~/gpt_summaries/io/summaries.txt | cut -f1 -d' ') + 1))
 done &
